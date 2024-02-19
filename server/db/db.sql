99802f0f7e1a6cda654a77b597cc98a7de2df4c5
Evgen-ia <yampolskaya.evgenie@yandex.ru>
2024-02-19 08:08:52 +0200

    db query

--- server/db/db.sql
+++ server/db/db.sql
@@ -1,3 +1,11 @@
+CREATE TABLE restaurants(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    price_range INT NOT NULL check(price_range >= 1 and price_range <= 5)
+);
+
+
 CREATE TABLE reviews(
     id BIGSERIAL NOT NULL PRIMARY KEY,
     restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
@@ -5,3 +13,4 @@ CREATE TABLE reviews(
     review TEXT NOT NULL,
     rating INT check(rating >=1 and ratif <= 5)
 );
+
--- server/server.js
+++ server/server.js
@@ -4,6 +4,22 @@ const db = require("./db/index.js")
 // const morgan = require("morgan");
 // const { query } = require("express");
 
+db.query("CREATE TABLE restaurants(\
+    id BIGSERIAL NOT NULL PRIMARY KEY,\
+    \"name\" VARCHAR(50) NOT NULL,\
+    \"location\" VARCHAR(50) NOT NULL,\
+    price_range INT NOT NULL check(price_range >= 1 and price_range <= 5)\
+  );\
+  CREATE TABLE reviews(\
+      id BIGSERIAL NOT NULL PRIMARY KEY,\
+      restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),\
+      name VARCHAR (50) NOT NULL,\
+      review TEXT NOT NULL,\
+      rating INT check(rating >=1 and ratif <= 5)\
+    );"
+
+);
+
 const cors = require("cors")
 
 // app.use(morgan("dev"));
@@ -22,6 +38,8 @@ const loggerMiddleware = (req, res, next) => {
     next();
 };
 app.use(loggerMiddleware);
+
+
 
 
 // get the list of restaurants
