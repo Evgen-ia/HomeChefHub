ed49ca96379f926216981f877ca93bcd1bc1800f
Evgen-ia <yampolskaya.evgenie@yandex.ru>
2024-02-19 10:16:45 +0200

    db query-3????

--- server/server.js
+++ server/server.js
@@ -23,22 +23,23 @@ const loggerMiddleware = (req, res, next) => {
 };
 app.use(loggerMiddleware);
 
-
-db.query("CREATE TABLE restaurants(\
-  id BIGSERIAL NOT NULL PRIMARY KEY,\
-  \"name\" VARCHAR(50) NOT NULL,\
-  \"location\" VARCHAR(50) NOT NULL,\
-  price_range INT NOT NULL check(price_range >= 1 and price_range <= 5)\
-);\
-CREATE TABLE reviews(\
+try{
+  await db.query("CREATE TABLE restaurants(\
     id BIGSERIAL NOT NULL PRIMARY KEY,\
-    restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),\
-    name VARCHAR (50) NOT NULL,\
-    review TEXT NOT NULL,\
-    rating INT check(rating >=1 and ratif <= 5)\
-  );"
+    \"name\" VARCHAR(50) NOT NULL,\
+    \"location\" VARCHAR(50) NOT NULL,\
+    price_range INT NOT NULL check(price_range >= 1 and price_range <= 5)\
+  );\
+  CREATE TABLE reviews(\
+      id BIGSERIAL NOT NULL PRIMARY KEY,\
+      restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),\
+      name VARCHAR (50) NOT NULL,\
+      review TEXT NOT NULL,\
+      rating INT check(rating >=1 and ratif <= 5)\
+    );"
 
-);
+  );
+}catch (err) { console.log(err); 
 
 // get the list of restaurants
 app.get("/api/v1/restaurants", async (req, res) => {
