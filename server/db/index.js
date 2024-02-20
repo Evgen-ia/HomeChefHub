2a419fc64103c0825bc71e207f0b5ef8ba2bae57
Evgen-ia <yampolskaya.evgenie@yandex.ru>
2024-02-20 21:17:53 +0200

    pool setting

--- server/db/index.js
+++ server/db/index.js
@@ -5,7 +5,7 @@ const { Pool } = require("pg");
 //     query: (text, params) => pool.query(text, params),
 // };
 
-const connectionString = 'postgres://username:password@hostname:port/database_name';
+const connectionString = 'postgres://restaurants_db_bjs4_user:eMiwZQllWGzZLnfBgMvXabszs6nfSKpL@dpg-cn9emdocmk4c739vlsd0-a/restaurants_db_bjs4';
 
 const pool = new Pool({
   connectionString: connectionString,
