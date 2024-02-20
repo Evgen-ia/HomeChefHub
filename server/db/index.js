6d912f4116fa3ea8e5322c5704f83ee23e1f90a3
Evgen-ia <yampolskaya.evgenie@yandex.ru>
2024-02-20 21:16:45 +0200

    pool setting

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,12 @@
 const { Pool } = require("pg");
 
-const pool = new Pool();
-module.exports = {
-    query: (text, params) => pool.query(text, params),
-};
+// const pool = new Pool();
+// module.exports = {
+//     query: (text, params) => pool.query(text, params),
+// };
+
+const connectionString = 'postgres://username:password@hostname:port/database_name';
+
+const pool = new Pool({
+  connectionString: connectionString,
+});
